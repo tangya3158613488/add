4e73b2d996f6c98351ec9e2ca851ac21dc879d64
tangya3158613488 <3158613488@qq.com>
2018-11-10 17:30:57 +0800

    提交代码

--- test_11_7/test_11_7/test.c
+++ test_11_7/test_11_7/test.c
@@ -1,46 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-//#include<stdio.h>
-//#include<windows.h>
-//void find_num(int arr[], int sz)
-//{
-//	int i = 0;//循环变量
-//	int ret = 0;//记录所有值异或的结果
-//	int pos = 0;//记录ret二进制为第一个为1的位置
-//	int x = 0;//将一组值异或在x，得到一个没有相同数字的值
-//	int y = 0; //将一组值异或在y，得到一个没有相同数字的值
-//	for (i = 0; i < sz; i++)
-//	{
-//		ret ^= arr[i];
-//	}
-//	for (i = 0; i < 32; i++)
-//	{
-//		if (1 == (ret >> i) & 1)
-//		{
-//			pos = i;
-//			break;
-//		}
-//	}
-//		for (i = 0; i < sz; i++)
-//		{
-//			if (1 == ((arr[i] >> pos) & 1))
-//			{
-//				x ^= arr[i];
-//			}
-//			else
-//				y ^= arr[i];
-//			
-//		}
-//		printf("x=%d,y=%d\n", x, y);
-//
-//}
-//int main()
-//{
-//	int arr[] = { 1, 2, 3, 1, 2, 3, 4, 5 };
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	find_num(arr, sz);
-//	return 0;
-//}
-//模拟实现strcap
+#include<stdio.h>
+#include<windows.h>
+void find_num(int arr[], int sz)
+{
+	int i = 0;//循环变量
+	int ret = 0;//记录所有值异或的结果
+	int pos = 0;//记录ret二进制为第一个为1的位置
+	int x = 0;//将一组值异或在x，得到一个没有相同数字的值
+	int y = 0; //将一组值异或在y，得到一个没有相同数字的值
+	for (i = 0; i < sz; i++)
+	{
+		ret ^= arr[i];
+	}
+	for (i = 0; i < 32; i++)
+	{
+		if (1 == (ret >> i) & 1)
+		{
+			pos = i;
+			break;
+		}
+	}
+		for (i = 0; i < sz; i++)
+		{
+			if (1 == ((arr[i] >> pos) & 1))
+			{
+				x ^= arr[i];
+			}
+			else
+				y ^= arr[i];
+			
+		}
+		printf("x=%d,y=%d\n", x, y);
+
+}
+int main()
+{
+	int arr[] = { 1, 2, 3, 1, 2, 3, 4, 5 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	find_num(arr, sz);
+	return 0;
+}
+模拟实现strcap
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
