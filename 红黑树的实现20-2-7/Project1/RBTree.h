fc42641e1d8ba03011752c79f670bb05c42c00ae
tangya3158613488 <3158613488@qq.com>
2020-02-07 23:58:00 +0800

    提交代码

--- 红黑树的实现20-2-7/Project1/RBTree.h
+++ 红黑树的实现20-2-7/Project1/RBTree.h
@@ -1,91 +1,89 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 #pragma once
 #include<iostream>
-using namespace std;
+
+using std::cout;
+using std::endl;
+
 enum Color
 {
 	RED,
 	BLACK
 };
-template<class K,class V>
+template<class T>
 struct RBTreeNode
 {
-	RBTreeNode<K,V>* _left;
-	RBTreeNode<K,V>* _right;
-	RBTreeNode<K,V>* _parent;
-	pair<K,V> _kv;
-	Color _color;
-	RBTreeNode(const pair<K,V>& kv)
+	RBTreeNode(const T& data = T(), Color color = RED)
 		: _left(nullptr)
 		, _right(nullptr)
 		, _parent(nullptr)
-		, _kv(kv)
-		, _color(RED)
+		, _data(data)
+		, _color(color)
 	{}
+	RBTreeNode<T>* _left;
+	RBTreeNode<T>* _right;
+	RBTreeNode<T>* _parent;
+	T _data;
+	Color _color;
 };
-template<class K,class V>
+template<class T>
 class RBTree
 {
-	typedef RBTreeNode<K,V> Node;
+	typedef RBTreeNode<T> Node;
 public:
 	RBTree()
 		:_root(nullptr)
 	{}
-	
 	~RBTree()
 	{
 		Destroy(_root);
-		_root = nullptr;
 	}
-	bool Insert(const pair<K, V>& kv)
+	bool Insert(const T& data)
 	{
-		if (_root == nullptr)
+		//第一步：按照二叉搜索树的方式插入新结点
+		if (nullptr == _root)
 		{
-			_root = new Node(kv);
+			_root = new Node(data);
 			_root->_color = BLACK;
 			return true;
 		}
 		else
 		{
+			//按照二叉搜索树的特性，在红黑树中找到要插入的位置
 			Node* cur = _root;
 			Node* parent = nullptr;
-			//在红黑树中找到结点要插入的位置
 			while (cur)
 			{
-				if (kv.first < cur->_kv.first)
+				if (data < cur->_data)
 				{
 					parent = cur;
 					cur = cur->_left;
-				}
-				else if (kv.first > cur->_kv.first)
+				}	
+				else if (data > cur->_data)
 				{
 					parent = cur;
 					cur = cur->_right;
 				}
 				else
-				{
 					return false;
-				}
 			}
 			//插入新结点
-			cur = new Node(kv);
-			cur->_color = RED;//新插入结点的颜色设定为红色
-			if (kv.first < parent->_kv.first)
+			cur = new Node(data);
+			cur->_color = RED;
+			if (data < parent->_data)
 			{
 				parent->_left = cur;
 				cur->_parent = parent;
-			}
+			}	
 			else
 			{
 				parent->_right = cur;
 				cur->_parent = parent;
 			}
-			//检测新结点插入后，红黑树的性质是否遭到破坏
-			//因为新结点的默认颜色是红色，因此：如果其双亲结点的颜色是黑色，没有违反红黑树的性质，不需要调整；
-			//但当新插入结点的双亲结点是红色时，就违反了性质三不能有连在一起的红色结点，此时需要对红黑树分情况讨论：
+			
+			//第二步：检测新结点插入后，红黑树的性质是否遭到破坏，若满足性质直接退出，否则对红黑树进行旋转着色处理
 			while (parent && RED == parent->_color)
 			{
-				//此时grandfather一定存在，因为parent存在，且不是黑色结点，则parent一定不是根
+				//此时grandfather一定存在，因为parent存在，且颜色不是黑色，则parent一定不是根
 				Node* grandfather = parent->_parent;
 				//parent在左侧的情况
 				if (parent == grandfather->_left)
@@ -105,20 +103,20 @@ public:
 						if (cur == parent->_right)
 						{
 							RotateL(parent);
-							swap(cur, parent);
+							std::swap(cur, parent);
 						}
 						grandfather->_color = RED;
 						parent->_color = BLACK;
 						RotateR(grandfather);
+						break;
 					}
 				}
 				//parent在右侧的情况
 				else
 				{
-					Node* uncle = grandfather->_right;
+					Node* uncle = grandfather->_left;
 					if (uncle && RED == uncle->_color)
 					{
-						//叔叔结点存在且为红色
 						parent->_color = uncle->_color = BLACK;
 						grandfather->_color = RED;
 						cur = grandfather;
@@ -126,11 +124,10 @@ public:
 					}
 					else
 					{
-						//叔叔结点不存在，或叔叔结点存在且为黑色
 						if (cur == parent->_left)
 						{
 							RotateR(parent);
-							swap(cur,parent);
+							std::swap(cur, parent);
 						}
 						RotateL(grandfather);
 						grandfather->_color = RED;
@@ -142,35 +139,6 @@ public:
 			_root->_color = BLACK;
 			return true;
 		}
-	}
-	void RotateR(Node* parent)
-	{
-		Node* subL = parent->_left;
-		Node* subLR = subL->_right;
-		parent->_left = subLR;
-		if (subLR)
-		{
-			subLR->_parent = parent;
-		}
-		subL->_right = parent;
-		Node* pparent = parent->_parent;
-		parent->_parent = subL;
-		if (parent == _root)
-		{
-			_root = subL;
-		}
-		else
-		{
-			if (pparent->_left == parent)
-			{
-				pparent->_left = subL;
-			}
-			else
-			{
-				pparent->_right = subL;
-			}
-		}
-		subL->_parent = pparent;
 	}
 	void RotateL(Node* parent)
 	{
@@ -201,42 +169,70 @@ public:
 		}
 		subR->_parent = pparent;
 	}
+	void RotateR(Node* parent)
+	{
+		Node* subL = parent->_left;
+		Node* subLR = subL->_right;
+		parent->_left = subLR;
+		if (subLR)
+		{
+			subLR->_parent = parent;
+		}
+		Node* pparent = parent->_parent;
+		subL->_right = parent;
+		parent->_parent = subL;
+		if (parent == _root)
+		{
+			_root = subL;
+		}
+		else
+		{
+			if (pparent->_left == parent)
+			{
+				pparent->_left = subL;
+			}
+			else
+			{
+				pparent->_right = subL;
+			}
+		}
+		subL->_parent = pparent;
+	}
 	void Inorder()
 	{
 		_Inorder(_root);
 		cout << endl;
 	}
-	bool IsRBtree()
-	{
-		if (nullptr == )
-	}
 private:
+	void _Inorder(Node* root)
+	{
+		if (root)
+		{
+			_Inorder(root->_left);
+			cout << root->_data << " ";
+			_Inorder(root->_right);
+		}
+	}
 	void Destroy(Node* root)
 	{
 		if (root)
 		{
 			Destroy(root->_left);
 			Destroy(root->_right);
-			delete root;
+			root = nullptr;
 		}
 	}
-	void _Inorder(Node* root)
-	{
-		if (root == nullptr)
-			return;
-		_Inorder(root->_left);
-		cout << root->_kv.first << " ";
-		_Inorder(root->_right);
-	}
+private:
 	Node* _root;
 };
 void TestRBTree()
 {
-	RBTree<int, int> t;
-	t.Insert(make_pair(1,1));
-	t.Insert(make_pair(3,3));
-	/*t.Insert(make_pair(2,2));
-	t.Insert(make_pair(5,5));*/
-
-	t.Inorder();
+	RBTree<int> rt1;
+	rt1.Insert(2);
+	rt1.Insert(8);
+	rt1.Insert(11);
+	rt1.Insert(13);
+	rt1.Insert(17);
+	rt1.Insert(15);
+	rt1.Inorder();
 }
---
+++ 红黑树的实现20-2-7/Project1/main.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"RBTree.h"
+
+int main()
+{
+	TestRBTree();
+	return 0;
+}
